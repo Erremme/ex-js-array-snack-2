e303b3c52c4e4e14778b8d2d5244c7ddcec88225
Erremme <Erremme2.0@gmail.com>
2025-05-13 16:05:33 +0200

    fix : added file script.js for api call

--- main.js
+++ main.js
@@ -112,23 +112,4 @@ console.log(agesSum);
 const averageAge = agesSum / ages.length;
 console.log(averageAge);
 
-/*
-Snack 5 (Bonus) - Raccogli i libri
-Usando la l'API https://boolean-spec-frontend.vercel.app/freetestapi/books/{id} usa la combinazione di .map() e Promise.all(), per creare una funzione (getBooks) che a partire da un array di id (ids), ritorna una promise che risolve un array di libri (books).
-Testala con l’array [2, 13, 7, 21, 19] .
 
-*/
-
-const ids = [2, 13, 7, 21, 19];
-async function getBooks(ids) {
-  const promises = ids.map(id => fetch(`http://localhost:5000/books/${id}`).then(response => response.json()));
-  const books = await Promise.all(promises);
-  return books;
-}
-getBooks(ids)
-  .then(books => {
-    console.log(books);
-  })
-  .catch(error => {
-    console.error('Error fetching books:', error);
-  });
---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+Snack 5 (Bonus) - Raccogli i libri
+Usando la l'API https://boolean-spec-frontend.vercel.app/freetestapi/books/{id} usa la combinazione di .map() e Promise.all(), per creare una funzione (getBooks) che a partire da un array di id (ids), ritorna una promise che risolve un array di libri (books).
+Testala con l’array [2, 13, 7, 21, 19] .
+
+*/
+
+const ids = [2, 13, 7, 21, 19];
+async function getBooks(ids) {
+  const promises = ids.map(id => fetch(`http://localhost:5000/books/${id}`).then(response => response.json()));
+  const books = await Promise.all(promises);
+  return books;
+}
+getBooks(ids)
+  .then(books => {
+    console.log(books);
+  })
+  .catch(error => {
+    console.error('Error fetching books:', error);
+  });
