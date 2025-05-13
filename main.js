91e07bee7574bb0daef0f4e078efbf4790bafbca
Erremme <Erremme2.0@gmail.com>
2025-05-13 15:42:12 +0200

    snack-3

--- main.js
+++ main.js
@@ -84,3 +84,18 @@ const fullPricedBook = discountedBooks.find(book => {
   return Number.isInteger(price);
 });
 console.log(fullPricedBook);
+
+/*
+Snack 3 - Ordinare gli Autori
+Creare un array (authors) che contiene gli autori dei libri.
+Crea una variabile booleana (areAuthorsAdults) per verificare se gli autori sono tutti maggiorenni.
+Ordina l’array authors in base all’età, senza creare un nuovo array.
+(se areAuthorsAdult è true, ordina in ordine crescente, altrimenti in ordine decrescente)
+*/
+
+const authors = books.map(book => book.author);
+console.log(authors);
+const areAuthorAdults = authors.every(author => author.age >= 18)
+console.log(areAuthorAdults);
+authors.sort((a,b)=> b.age - a.age);
+console.log(authors);
