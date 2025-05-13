c4112e5caffafa4bb02e30febc98c64ac3533c88
Erremme <Erremme2.0@gmail.com>
2025-05-13 15:28:53 +0200

    snack-2

--- main.js
+++ main.js
@@ -62,3 +62,25 @@ const longBooks = books.filter(book => book.pages > 300);
 console.log(longBooks);
 const longBooksTitles = longBooks.map(book => book.title);
 console.log(longBooksTitles);
+
+/*
+Snack 2 - Il primo libro scontato
+Creare un array (availableBooks) che contiene tutti i libri disponibili.
+Crea un array (discountedBooks) con gli availableBooks, ciascuno con il prezzo scontato del 20% (mantieni lo stesso formato e arrotonda al centesimo)
+Salva in una variabile (fullPricedBook) il primo elemento di discountedBooks che ha un prezzo intero (senza centesimi).
+*/
+const availableBooks = books.filter(book => book.available);
+console.log(availableBooks);
+const discountedBooks = availableBooks.map(book => {
+  const price = parseFloat(book.price.replace('€', ''));
+  const discountedPrice = (price * 0.8).toFixed(2) + '€';
+  return { ...book, price: discountedPrice };
+});
+
+console.log(discountedBooks);
+
+const fullPricedBook = discountedBooks.find(book => {
+  const price = parseFloat(book.price.replace('€', ''));
+  return Number.isInteger(price);
+});
+console.log(fullPricedBook);
