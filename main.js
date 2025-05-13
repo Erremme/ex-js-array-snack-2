1cc52bdfd42794dc7ee950bb0a5bc45c1ec15dcb
Erremme <Erremme2.0@gmail.com>
2025-05-13 15:19:59 +0200

    snack-1

--- main.js
+++ main.js
@@ -53,3 +53,12 @@ Crea un array (longBooks) con i libri che hanno più di 300 pagine;
 Creare un array (longBooksTitles) che contiene solo i titoli dei libri contenuti in longBooks.
 Stampa in console ogni titolo nella console.
 */
+
+function sum(a, b) {
+  return a + b;
+}
+
+const longBooks = books.filter(book => book.pages > 300);
+console.log(longBooks);
+const longBooksTitles = longBooks.map(book => book.title);
+console.log(longBooksTitles);
